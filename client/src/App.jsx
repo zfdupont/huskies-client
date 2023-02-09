77f66d2b6a0f90a1abf2b223bd8d18f220b6bc88
zfdupont <zachary.dupont@stonybrook.edu>
2023-02-09 17:38:27 -0500

    removed extra packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Map from 'react-map-gl';
+import * as React from 'react';
+import mapboxgl from 'mapbox-gl';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [viewState, setViewState] = React.useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 3.5
+  });
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  React.useEffect(() => {
+    if(viewState.latitude < 24.52) viewState.latitude = 24.52
+    if(viewState.latitude > 49.38) viewState.latitude = 49.38
+    if(viewState.longitude < 66.95) viewState.latitude = 66.95
+    if(viewState.longitude > 124.77) viewState.latitude = 124.77 
+  }, [map, viewState])
+
+  var map = <Map
+    {...viewState}
+    mapboxAccessToken='pk.eyJ1IjoiaHVza2llczRsIiwiYSI6ImNsZHc4YjA2ajA0dDIzcG40MHk4Y3hoenEifQ.RVPl4GldZwjngT4CGN12Iw'
+    onMove={evt => setViewState(evt.viewState)}
+    mapStyle="mapbox://styles/mapbox/light-v11"
+    style={{width: 1440, height: 1024}}
+  />
+  return map;
 }
 
 export default App
