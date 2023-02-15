3ed7d4bf6e8d8c2b9e5fa93f7b13e576af07788e
Hana Mohideen <afreenmohideen@gmail.com>
2023-02-15 17:33:06 -0500

    adding graphs to state views

--- client/src/NewYork.jsx
+++ client/src/NewYork.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import shape from './data/shape.json';
 import {MapContainer, GeoJSON} from 'react-leaflet';
+import Bar from './bar';
 export default class NewYork extends React.Component{
     render() {
         return(
@@ -8,6 +9,9 @@ export default class NewYork extends React.Component{
                 <MapContainer center={[42.8, -76]} zoom={6}>
                     <GeoJSON style = {this.mapStyle} data={shape.features}/>
                 </MapContainer>
+                <div>
+                    <Bar></Bar>
+                </div>
             </div>
         )
     }
