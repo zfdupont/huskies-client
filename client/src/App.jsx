a35bebd233570329fd5abc38f5edfd20e61dac24
Jem Chen <jem.chen@stonybrook.edu>
2023-02-15 15:46:29 -0500

    leaflet map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+import React from 'react';
+import Map from './Map.jsx';
+import NewYork from './NewYork.jsx';
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {page:"Map"};
+        this.handlePage = this.handlePage.bind(this);
+    }
+    handlePage(page) {
+        this.setState({page: page});
+    }
+    render() {
+        var currPage = null;
+        switch(this.state.page) {
+            case "Map":
+                currPage = <Map handlePage = {this.handlePage}
+                />;
+                break;
+            case "NY":
+                currPage = <NewYork/>;
+                break;
+            default:
+                currPage = null;
+        }
+        return (
+            <div id = "main">
+                {currPage}
+            </div>
+        )
+    }
 }
-
-export default App
---
+++ client/src/NewYork.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import shape from './data/shape.json';
+import {MapContainer, GeoJSON} from 'react-leaflet';
+export default class NewYork extends React.Component{
+    render() {
+        return(
+            <div>
+                <MapContainer center={[42.8, -76]} zoom={6}>
+                    <GeoJSON style = {this.mapStyle} data={shape.features}/>
+                </MapContainer>
+            </div>
+        )
+    }
+}
