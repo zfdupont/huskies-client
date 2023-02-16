192a17538c93cea0bca1652c569ead8ed294e481
zfdupont <zachary.dupont@stonybrook.edu>
2023-02-15 21:58:58 -0500

    map fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
-import React, { Context, Consumer } from 'react';
+import React, { Context, Consumer, useState } from 'react';
 import Map from './Map.jsx';
+import {Menu, MenuItem, FormControl, InputLabel, Select} from '@mui/material'
 
 export default function App(){
-    
+    const [state, setState] = useState("");
+    let handleChange = (e) => {
+        setState(e.target.value);
+    }
     return (
         <>
+            <div id='header'>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">State</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={state}
+                        label="State"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={'Georgia'}>Georgia</MenuItem>
+                        <MenuItem value={'Illinois'}>Illinois</MenuItem>
+                        <MenuItem value={'New York'}>New York</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div id='main'>
+                <Map state={state}></Map>
+                <div id='stats'>STATS HERE</div>
+            </div>
             
-            <Map></Map>
         </>
     );
 }
--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,15 +1,74 @@
-import React from 'react';
-import {MapContainer, GeoJSON, TileLayer, Marker, Popup} from 'react-leaflet';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import GAD from './data/GAD.json';
+import ILD from './data/ILD.json';
+import NYD from './data/NYD.json';
+import {MapContainer, GeoJSON, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
+
+
+function getJsonByName(name) {
+    let id = '';
+    switch(name){
+        case 'Georgia':
+            return GAD;
+        case 'Illinois':
+            return ILD;
+        case 'New York':
+            return NYD;
+    }
+}
+
+function FlyMapTo(props){
+    const map = useMap();
+    if (props.state){
+        const json = getJsonByName(props.state);
+        console.log(json);
+        var layer = new L.geoJSON(json);
+    }
+    
+    useEffect(() => {
+        map.flyTo(props.position, props.zoom);
+        if(props.state) {
+            let currLayer = null;
+            map.eachLayer((l) => {
+                if(l instanceof L.GeoJSON)
+                    currLayer = l;
+            });
+            if(currLayer) map.removeLayer(currLayer);
+            map.addLayer(layer);
+        }
+    });
+    return null;
+}
 
 export default function Map(props){
     let position = [39.8283, -98.5795];
+    let zoom = 5;
+    switch (props.state) {
+        case 'Georgia':
+            zoom = 6;
+            position = [32.428, -83.297];
+            break;
+        case 'Illinois':
+            zoom = 6;
+            position = [40.08, -89.184];
+            break;
+        case 'New York':
+            zoom = 6;
+            position = [42.579, -76.10];
+            break;
+        default:
+            break;
+    }
+    console.log(position);
     return (
         <div id='map'>
-            <MapContainer center={position} zoom={5} scrollWheelZoom={false}>
+            <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <FlyMapTo position={position} zoom={zoom} state={props.state}/>
             </MapContainer>
         </div>
     );
