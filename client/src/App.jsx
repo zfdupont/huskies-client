8ac4f38afe81e53a8fe579accc55d703f7407158
zfdupont <zachary.dupont@stonybrook.edu>
2023-02-15 20:29:57 -0500

    a lot of deletions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,12 @@
-import React from 'react';
+import React, { Context, Consumer } from 'react';
 import Map from './Map.jsx';
-import NewYork from './NewYork.jsx';
-import Georgia from './Georgia.jsx';
-import Illinois from './Illinois.jsx';
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {page:"Map"};
-        this.handlePage = this.handlePage.bind(this);
-    }
-    handlePage(page) {
-        this.setState({page: page});
-    }
-    render() {
-        var currPage = null;
-        switch(this.state.page) {
-            case "Map":
-                currPage = <Map handlePage = {this.handlePage}
-                />;
-                break;
-            case "NY":
-                currPage = <NewYork/>;
-                break;
-            case "GA":
-                currPage = <Georgia/>;
-                break;
-            case "IL":
-                currPage = <Illinois/>;
-                break;
-            default:
-                currPage = null;
-        }
-        return (
-            <div id = "main">
-                {currPage}
-            </div>
-        )
-    }
+
+export default function App(){
+    
+    return (
+        <>
+            
+            <Map></Map>
+        </>
+    );
 }
--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,56 +1,16 @@
 import React from 'react';
-import states from './data/states.json';
-import NY from './data/NY.json';
-import GA from './data/GA.json';
-import IL from './data/IL.json';
-import {MapContainer, GeoJSON} from 'react-leaflet';
-export default class Map extends React.Component{
-	constructor(props) {
-		super(props);
-        this.stateSelect = this.stateSelect.bind(this);
-        this.selectNY = this.selectNY.bind(this);
-        this.selectGA = this.selectGA.bind(this);
-        this.selectIL = this.selectIL.bind(this);
-    }
-    mapStyle = {
-        fillColor: "yellow",
-        color: "black",
-        weight: 2
-    };
-    stateStyle = {
-        fillColor: "green",
-        fillOpacity: 0.5,
-        color: "black",
-        weight: 2
-    };
-    selectNY() {
-        this.props.handlePage("NY");
-    }
-    selectGA() {
-        this.props.handlePage("GA");
-    }
-    selectIL() {
-        this.props.handlePage("IL");
-    }
-    stateSelect() {
-        document.getElementById("dropdown").classList.toggle("show");
-    };
-    render() {
-        return(
-            <div>
-                <MapContainer center={[37.4316, -78.6569]} zoom={4}>
-                    <GeoJSON style={this.stateStyle} data={NY.geometry}/>
-                    <GeoJSON style={this.stateStyle} data={GA.geometry}/>
-                    <GeoJSON style={this.stateStyle} data={IL.geometry}/>
-                    <GeoJSON style={this.mapStyle} data={states.features}/>
-                </MapContainer>
-                <button onClick = {this.stateSelect}>Select State</button>
-                <div id = "dropdown" class = "dropdown_states">
-                    <a href="#" onClick = {this.selectNY}>New York</a>
-                    <a href="#" onClick = {this.selectGA}>Georgia</a>
-                    <a href="#" onClick = {this.selectIL}>Illinois</a>
-                </div>
-            </div>
-        )
-    }
+import {MapContainer, GeoJSON, TileLayer, Marker, Popup} from 'react-leaflet';
+
+export default function Map(props){
+    let position = [39.8283, -98.5795];
+    return (
+        <div id='map'>
+            <MapContainer center={position} zoom={5} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+    );
 }
