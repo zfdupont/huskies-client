5e2765fa33a6e5c3bec7d671cb37936773c8d32b
zfdupont <zachary.dupont@stonybrook.edu>
2023-02-22 18:44:14 -0500

    server folder init

---
+++ server/src/test/java/com/huskies/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huskies.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
