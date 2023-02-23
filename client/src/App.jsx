2780a4613f65ac0340e6ace759a3442b5d17eb1e
Yongjung Shin <yongshn220@gmail.com>
2023-02-22 23:26:33 -0500

    Init frontend branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import Map from './Map.jsx';
-import NewYork from './NewYork.jsx';
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {page:"Map"};
-        this.handlePage = this.handlePage.bind(this);
-    }
-    handlePage(page) {
-        this.setState({page: page});
-    }
-    render() {
-        var currPage = null;
-        switch(this.state.page) {
-            case "Map":
-                currPage = <Map handlePage = {this.handlePage}
-                />;
-                break;
-            case "NY":
-                currPage = <NewYork/>;
-                break;
-            default:
-                currPage = null;
-        }
-        return (
-            <div id = "main">
-                {currPage}
-            </div>
-        )
-    }
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+    <BrowserRouter>
+        <AppBanner />
+    </BrowserRouter>
 }
+
+export default App;
