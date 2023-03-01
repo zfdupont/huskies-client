10a0cc793fc6734be5742dd9f40c7ad75053e4e4
Hana Mohideen <afreenmohideen@gmail.com>
2023-03-01 06:21:48 -0500

    bar charts and whisker plot

--- client/src/TabPanels/analyzePanel/AnalyzePanel.js
+++ client/src/TabPanels/analyzePanel/AnalyzePanel.js
@@ -1,11 +1,20 @@
 
 import * as React from "react";
+import { Bar } from "recharts";
+import './AnalyzePanel.css'
+import BarChartComponent from "./BarChartComponent";
+import Whisker from "./Whisker.tsx";
 
 export default function AnalyzePanel()
 {
     return (
-        <div style={{position:'absolute', width:'100%', height:'100%', backgroundColor:'red'}}>
-            This is Analyze Component.
+        <div id='Panel'>
+            <div id='barChart'>
+                <BarChartComponent></BarChartComponent>
+                <BarChartComponent></BarChartComponent>
+                <BarChartComponent style={{padding: 500 }}></BarChartComponent>
+            </div>
+            <Whisker></Whisker>
         </div>
     );
 }
