39a356a82ebfbd3ddf25dd5ac9f3bc49fc87a069
Jem Chen <jem.chen@stonybrook.edu>
2023-03-06 17:16:30 -0500

    renamed Georgia column names

--- scripts/mergeDataGA.py
+++ scripts/mergeDataGA.py
@@ -10,10 +10,12 @@ edata = pd.read_csv("./data/GA/ga_2020_2020_vtd.csv")
 edata = edata[['GEOID20','G20PRERTRU','G20PREDBID']]
 #reading census data, filter out geoid, area, coordinates, demographic data
 ddata = pd.read_csv("./data/GA/ga_pl2020_vtd.csv")
-ddata = ddata[['GEOID20','AREALAND','AREAWATR','INTPTLAT','INTPTLON','P0010001','P0010002','P0010003','P0010004','P0010005','P0010006','P0010007','P0010008','P0020002','P0020003']]
+ddata = ddata[['GEOID20','P0030001','P0030003','P0030004','P0030005','P0030006','P0030007','P0030008','P0030009','P0040002']]
 #merging data sets on GEOID20
 gdf  = edata.merge(gdf, on='GEOID20', how='left')
 gdf  = ddata.merge(gdf, on='GEOID20', how='left')
-#convert to geoJSON
+#store in MongoDB
+gdf = gdf.rename(columns={'G20PRERTRU': '2020VTRUMP', 'G20PREDBID': '2020VBIDEN', 'P0030001':'VAPTOTAL', 'P0030003':'VAPWHITE', 'P0030004':'VAPBLACK','P0030005':'VAPINAMORAK','P0030006':'VAPASIAN','P0030007':'VAPISLAND','P0030008':'VAPOTHER','P0030009':'VAPMIXED','P0040002':'VAPHISP'})
 gdf = gpd.GeoDataFrame(gdf, geometry='geometry')
-gdf.to_file('precinctDataGA.geojson', driver='GeoJSON')
+
+gdf.to_file('mergedGA.geojson', driver='GeoJSON')
