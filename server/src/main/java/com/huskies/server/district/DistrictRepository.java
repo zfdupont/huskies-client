bca5bd61ad60c83b44bd9339021a605234521bd7
zfdupont <zachary.dupont@stonybrook.edu>
2023-03-22 14:14:18 -0400

    moving backend to new repo

--- server/src/main/java/com/huskies/server/district/DistrictRepository.java
+++
@@ -1,8 +0,0 @@
-package com.huskies.server.district;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistrictRepository extends CrudRepository<District, String> {
-}
--- server/src/main/java/com/huskies/server/precinct/PrecinctRepository.java
+++
@@ -1,7 +0,0 @@
-package com.huskies.server.precinct;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
-@Repository
-public interface PrecinctRepository extends CrudRepository<Precinct, String> {
-}
