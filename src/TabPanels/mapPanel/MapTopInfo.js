dac5738ae25b93d7a016aa8e723e951d7758f008
yongshn220 <96238634+yongshn220@users.noreply.github.com>
2023-04-10 19:48:58 -0400

    G UI 13 (#22)
    * Add | district summary table
    
    * Indent

--- src/TabPanels/mapPanel/MapTopInfo.js
+++ src/TabPanels/mapPanel/MapTopInfo.js
@@ -1,35 +1,33 @@
 import * as React from "react";
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 
 export default function MapTopInfo()
 {
     return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', position:'relative',left:'5%', top:'-100%', width:'90%', height:'50px', fontWeight:'500', fontSize:'12px', color:'white', zIndex: 701}}>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>1</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>3</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>5</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>7</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>9</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>11</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>13</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>15</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>17</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>19</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>21</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>23</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#0984e3'}}>25</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>2</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>4</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>6</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>8</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>10</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>12</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>14</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>16</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>18</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>20</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>22</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>24</div>
-            <div style={{display: 'flex', alignItems: 'center',justifyContent: 'center', minWidth:'20px', maxWidth:'25px', minHeight:'20px', maxHeight:'25px', margin: '3px', borderRadius: '20px', backgroundColor: '#d63031'}}>26</div>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', position:'relative',left:'30%', top:'-98%', width:'40%', height:'50px', fontWeight:'500', fontSize:'12px', color:'white', zIndex: 701}}>
+            <TableContainer component={Paper}>
+                <Table size="small" sx={{ minWidth: 200}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ fontSize: 12 }} align="center">Total Districts</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">Dem</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">Rep</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">Incumbents</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow
+                            key={'summary'}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell sx={{ fontSize: 12 }} align="center">10</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">10</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">10</TableCell>
+                            <TableCell sx={{ fontSize: 12 }} align="center">10</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
