242cacdd359e22f260f3b90c18c749b4ee6f78f0
Hana Mohideen <afreenmohideen@gmail.com>
2023-05-07 00:29:59 -0400

    GUI-18

--- src/TabPanels/analyzePanel/ChartBox.js
+++ src/TabPanels/analyzePanel/ChartBox.js
@@ -5,6 +5,7 @@ import {useContext, useState, useEffect} from "react";
 import StoreContext from "../../common/Store";
 import TestChart from "./TestChart";
 import BarChart from "./BarChart";
+import SafeSeats from "./SafeSeats";
 
 const ChartButtonType = {
     BW: "box-and-whisker",
@@ -13,7 +14,7 @@ const ChartButtonType = {
 
 export default function ChartBox()
 {
-    const { dataStore } = useContext(StoreContext);
+    const { mapStore, dataStore } = useContext(StoreContext);
     const [ state, setState ] = useState({
         selectedChart: ChartButtonType.BW,
         dataReady: false,
@@ -31,13 +32,17 @@ export default function ChartBox()
     function getDistrictSummaryInfo() {
         const boxWhiskerChart = [];
         if (!dataStore.isEnsemblejsonReady()) return boxWhiskerChart;
-        //let stateModelData = dataStore.getStateModelData(mapStore.getMapPlan(), mapStore.getState());
+        let stateModelData = dataStore.getStateModelData(mapStore.getMapPlan(), mapStore.getState());
         let allGraphData = dataStore.getEnsembleData();
+        let safeSeatsData = generateSafeSeatsData(stateModelData.electionDataDict);
         let bw_data = allGraphData.box_w_data;
         if(bw_data) {
             boxWhiskerChart.push(<TestChart key={1} data={bw_data}/>);
         }
-        console.log(bw_data);
+        if(safeSeatsData) {
+            boxWhiskerChart.push(<SafeSeats key={2} data={safeSeatsData}/>);
+        }
+        //console.log(bw_data);
         // for (let id in stateModelData.electionDataDict) {
         //     if (state.incumbentFilter && !stateModelData.electionDataDict[id].hasIncumbent) continue;
 
@@ -56,6 +61,33 @@ export default function ChartBox()
         //ahh how should we do this...load all data at the beginning and then load charts as necessary?
         //or request data when needed? it's already being computed anyways so it makes more sense to
         //to load all at once...
+    }
+
+    function generateSafeSeatsData(election_data) {
+        let safe_seats_data = {'incumbent': 0, 'open_seat': 0, 'dem-incmb': 0, 'rep-incmb': 0, 'dem-open': 0, 'rep-open': 0};
+        for (let id in election_data) {
+            let district = election_data[id];
+            if (district.hasIncumbent) {
+                safe_seats_data['incumbent'] +=1;
+                if(district.winnerParty === 'democratic') {
+                    safe_seats_data['dem-incmb'] += 1;
+                }
+                else {
+                    safe_seats_data['rep-incmb'] += 1;
+                }
+            } 
+            else {
+                safe_seats_data['open_seat'] += 1;
+                if(district.winnerParty === 'democratic') {
+                    safe_seats_data['dem-open'] += 1;
+                }
+                else {
+                    safe_seats_data['rep-open'] += 1;
+                }
+            } 
+        }
+        console.log(safe_seats_data);
+        return safe_seats_data;
     }
 
     function getBoxAndWhiskerData() {
@@ -83,8 +115,9 @@ export default function ChartBox()
                 </div>
             ) : <div></div>} */}
             {/* {bwdata} */}
-            {/* {boxWhiskerChart} */}
-            <BarChart></BarChart>
+            {boxWhiskerChart[1]}
+            {/* <BarChart></BarChart> */}
+            {/* <SafeSeats></SafeSeats> */}
         </div>
     );
 }
--- src/TabPanels/analyzePanel/SummaryEnsembleTable.js
+++ src/TabPanels/analyzePanel/SummaryEnsembleTable.js
@@ -1,15 +1,13 @@
 import * as React from "react";
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {useContext, useState, useEffect} from "react";
+import {useContext} from "react";
 import StoreContext from "../../common/Store";
 
 export default function SummaryEnsembleTable() {
 
     const { dataStore } = useContext(StoreContext);
 
-
     let ensembleTable = getEnsembleSummaryData();
-    //let table = getSummaryEnsembleTable();
 
     function getEnsembleSummaryData() {
         const ensembleTable = [];
@@ -18,17 +16,9 @@ export default function SummaryEnsembleTable() {
         let allGraphData = dataStore.getEnsembleData();
         let summary_data = allGraphData.ensemble_summary;
         if(summary_data) {
-            console.log(summary_data);
             let table = getSummaryEnsembleTable(summary_data);
             ensembleTable.push(table);
         }
-        //console.log(bw_data);
-        // for (let id in stateModelData.electionDataDict) {
-        //     if (state.incumbentFilter && !stateModelData.electionDataDict[id].hasIncumbent) continue;
-
-        //     boxWhiskerChart.push(<TestChart data={bw_data}} />);
-        //     boxWhiskerChart.push(<DistrictSummaryItem key={id} electionData={stateModelData.electionDataDict[id]}/>);
-        // }
         return ensembleTable;
     }
 
