ff435bc12575160717eb4880e61273d57326ce4c
zfdupont <zachary.dupont@stonybrook.edu>
2026-09-17 04:39:12 -0400

    Remove dead district geojson files, cutting bundle ~90%
    NYD/GAD/ILD.json (31.7MB) were assigned to GeoData[state][DISTRICT] but
    never read; district borders come from /api/plan. Removing them and the
    unused DISTRICT enum key shrinks the JS bundle from 12.1MB to 1.3MB.

--- src/common/GlobalVariables.js
+++ src/common/GlobalVariables.js
@@ -1,9 +1,6 @@
 import NY from "../0.data/NY.json";
-import NYD from "../0.data/NYD.json";
 import GA from "../0.data/GA.json";
-import GAD from "../0.data/GAD.json"
 import IL from "../0.data/IL.json";
-import ILD from "../0.data/ILD.json";
 
 
 export const MapActionType = {
@@ -105,7 +102,6 @@ export const LayerGroupType = {
 // @enum {string}
 export const GeoDataType = {
     STATE: "state",
-    DISTRICT: "district",
     PRECINCT: "precinct",
 }
 
@@ -197,15 +193,12 @@ export const outlineColors = [
 export const GeoData = {}
 GeoData[StateType.NEWYORK] = {}
 GeoData[StateType.NEWYORK][GeoDataType.STATE] = NY;
-GeoData[StateType.NEWYORK][GeoDataType.DISTRICT] = NYD;
 
 GeoData[StateType.GEORGIA] = {}
 GeoData[StateType.GEORGIA][GeoDataType.STATE] = GA;
-GeoData[StateType.GEORGIA][GeoDataType.DISTRICT] = GAD;
 
 GeoData[StateType.ILLINOIS] = {}
 GeoData[StateType.ILLINOIS][GeoDataType.STATE] = IL;
-GeoData[StateType.ILLINOIS][GeoDataType.DISTRICT] = ILD;
 
 export const availableStateTypes = [
     StateType.NEWYORK,
