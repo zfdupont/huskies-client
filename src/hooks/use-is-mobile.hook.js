5886f76457b3cd321911c9a6e7d4e1a96025617d
zfdupont <zachary.dupont@stonybrook.edu>
2026-09-17 00:16:46 -0400

    Add pnpm build-script config; drop lodash from useIsMobile
    - pnpm-workspace.yaml disallows core-js/esbuild install scripts (silences ERR_PNPM_IGNORED_BUILDS)
    - Replace lodash/debounce import with a small inline debounce; fix resize listener cleanup

---
+++ src/hooks/use-is-mobile.hook.js
@@ -0,0 +1,25 @@
+import { useLayoutEffect, useState } from 'react';
+
+const debounce = (fn, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
+
+const useIsMobile = ()  => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useLayoutEffect(() => {
+    const updateSize = debounce(() => {
+      setIsMobile(window.innerWidth < 768);
+    }, 250);
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
