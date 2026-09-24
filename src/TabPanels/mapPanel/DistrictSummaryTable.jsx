176ab63ab16ed5ffc5afed616225b56867412735
zfdupont <zachary.dupont311@gmail.com>
2026-09-23 21:35:12 -0700

    Polish: drop dead comment; eof newline; warn on unexpected schema_version

--- src/TabPanels/mapPanel/DistrictSummaryTable.jsx
+++ src/TabPanels/mapPanel/DistrictSummaryTable.jsx
@@ -32,7 +32,6 @@ export default function DistrictSummaryTable() {
         let stateModelData = dataStore.getStateModelData(mapStore.getMapPlan(), mapStore.getState());
         for (let id in stateModelData.electionDataDict) {
             if (state.incumbentFilter && !stateModelData.electionDataDict[id].hasIncumbent) continue;
-            //console.log(ensembleData.enacted_data);
             districtSummaryInfo.push(<DistrictSummaryItem key={id} electionData={stateModelData.electionDataDict[id]} incumbentBundle={incumbentsByName[stateModelData.electionDataDict[id].incumbent]}/>);
         }
         return districtSummaryInfo;
